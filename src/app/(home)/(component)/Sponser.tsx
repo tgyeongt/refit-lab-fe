2d87bd9caa984059716451fe87fe5a55a104fce4
tgyeongt <tgyeongt@gmail.com>
2025-11-25 16:49:46 +0900

    fix: 이미지 반응형으로 조절

--- src/app/(home)/(component)/Sponser.tsx
+++ src/app/(home)/(component)/Sponser.tsx
@@ -3,12 +3,12 @@ import SponserImg from "@/assets/image/sponser.png";
 
 export default function Sponser() {
   return (
-    <div className="my-[30px] relative w-screen h-[50vw]">
+    <div className="my-[30px] w-full max-w-[700px] mx-auto">
       <Image
         src={SponserImg}
         alt="스폰서 이미지"
-        fill
-        className="object-cover"
+        className="w-full h-auto object-contain"
+        priority
       />
     </div>
   );
