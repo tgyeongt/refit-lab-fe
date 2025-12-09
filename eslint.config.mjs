b7c56436520bff44997a466521fd2248d14eb33a
seongmin36 <whtjdals3625@gmail.com>
2025-12-09 17:39:30 +0900

    fix: 모듈 참조 누락 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactHooks from "eslint-plugin-react-hooks";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
