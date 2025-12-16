732fc56923a47f530432df6ca79410a8c69d867d
tgyeongt <tgyeongt@gmail.com>
2025-12-16 18:55:55 +0900

    fix: 커뮤니티 대댓글 좌측 패딩 조정

--- src/app/community/[id]/(component)/CommentItem.tsx
+++ src/app/community/[id]/(component)/CommentItem.tsx
@@ -25,7 +25,7 @@ export default function CommentItem({
   };
 
   return (
-    <div style={{ marginLeft: level * 20 }} className="flex flex-col gap-2">
+    <div style={{ marginLeft: level * 30 }} className="flex flex-col gap-2">
       <CommentBlock
         commentId={comment.commentId}
         userProfile={comment.userProfile}
