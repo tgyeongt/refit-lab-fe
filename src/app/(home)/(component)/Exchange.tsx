5925a5c46aeb5b4909921f58df26d043f26ce5fa
tgyeongt <tgyeongt@gmail.com>
2025-12-16 13:38:20 +0900

    feat: 홈 화면 클립 배경 넣기

--- src/app/(home)/(component)/Exchange.tsx
+++ src/app/(home)/(component)/Exchange.tsx
@@ -6,6 +6,8 @@ import {
   getExchangeList,
   ExchangePost,
 } from "@/app/exchange/(api)/getExchangeList";
+import ClipImg from "@/assets/image/bg-clip.png";
+import Link from "next/link";
 
 export default function Exchange() {
   const [items, setItems] = useState<ExchangePost[]>([]);
@@ -50,14 +52,33 @@ export default function Exchange() {
     return <p className="text-center mt-10">게시글이 없습니다.</p>;
 
   return (
-    <div className="px-4 pt-6">
-      <div className="flex gap-4 overflow-x-auto h-[280px] scrollbar-hide snap-x snap-mandatory">
-        {items.map((item) => (
-          <div key={item.exchangePostId} className="snap-start">
-            <ExchangeCard key={item.exchangePostId} item={item} />
-          </div>
-        ))}
+    <section
+      id={"exchange"}
+      style={{
+        backgroundImage: `url(${ClipImg.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="w-full min-h-[490px] mt-[30px]"
+    >
+      <div className="flex items-center justify-between px-[15px] pt-[30px]">
+        <h2 className="text-[24px] font-semibold">지금 교환 중인 옷</h2>
+        <Link href="/exchange" className="text-[14px] text-[#9E9E9E]">
+          더보기
+        </Link>
       </div>
-    </div>
+
+      <div className="mt-2">
+        <div className="px-4 pt-6">
+          <div className="flex gap-4 overflow-x-auto h-[280px] scrollbar-hide snap-x snap-mandatory">
+            {items.map((item) => (
+              <div key={item.exchangePostId} className="snap-start">
+                <ExchangeCard key={item.exchangePostId} item={item} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
